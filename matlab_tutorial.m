6feb52cfa5086c1ef2a0cb65e78fe6d2a6a3ed53
Tahir Can Kozan <93403102+tahircankzn@users.noreply.github.com>
2024-05-25 11:35:13 +0300

    Update matlab_tutorial.m

--- matlab_tutorial.m
+++ matlab_tutorial.m
@@ -58,7 +58,7 @@ c1 = double(s1) % kökleri sayısal hale getirdi
 
 %Yöntem 2
 
-f2 = [-1 2 1 2 6]; % katsayıları yazdık
+f2 = [-1 2 1 2 6]; % katsayıları yazdık , polinom olduğu için 
 c2 = roots(f2)
 
 %%
@@ -117,3 +117,140 @@ x7 = ilaplace(1/((s^2)+1))
 %%
 % Transfer Fonksiyonu
 
+clear all
+close all
+clc
+
+% Örnek a
+
+numA = 100;
+denumA = [1 2 10 20 1];
+
+tfA = tf(numA,denumA) % tf() => transfer fonksiyonu , ilk değer pay , ikinci ise payda
+
+% 35.56
+
+%%
+
+syms M fv s
+
+ilaplace(1/((s.^2).*2 + 3.*s))
+
+
+%%
+x = [-1.89 -1.50 -1.11 -0.71 -0.32 -0.13 0.07];
+y = [0 0.08 0.58 1.50 2.83 3.64 4.54];
+
+plot(x,y)
+
+xlabel("Vgs");
+ylabel("Id");
+
+
+%%
+
+x = [0.07 1.06 2.04 2.23 2.51 2.98 3.47];
+y = [0 0 2.74 9.47 26 63.8 65.5];
+
+plot(x,y)
+
+xlabel("Vgs");
+ylabel("Id");
+
+%%
+
+y = [4.24 8.45 16.7 25.3 26.7 27.7]; % ıd
+
+% vgg 2.5
+x1 = [0.06 0.14 0.32 2.43 6.13 8.91]; % vds
+
+% vgg 3
+x2 = [0.04 0.08 0.18 0.39 0.72 0.91]; % vds
+
+% vgg 3.5
+x3 = [0.03 0.07 0.14 0.29 0.45 0.58]; % vds
+
+
+figure
+
+
+plot(x1,y)
+
+hold on 
+plot(x2,y)
+
+hold on 
+plot(x3,y)
+
+legend('Vgg 2.5', 'Vgg 3' , 'Vgg 3.5' );
+
+xlabel("Vds");
+ylabel("Id");
+
+hold off
+
+
+
+%%
+
+x = [-50:0.1:50];
+
+
+plot3(sin(x),x.^3+2.*x+5,x)
+
+%%
+
+x = [-10:0.1:10];
+
+func = 7.*sin(400.*pi.*x);
+
+plot(x,func)
+
+
+%%
+
+x = [-10:0.1:10];
+
+func = 7.*sin(400.*pi.*x);
+
+stem(x,func)
+
+%%
+
+x=-pi/2:0.005:pi/2;
+y=(0.64*sqrt(abs(x))-0.8+1.2.^abs(x).*cos(200*x)).*sqrt(cos(x));
+plot(x,y,'r')
+
+
+%%
+
+x = [100 200 500 1000 5000 20000 100000 500000 1000000 2000000 5000000 10000000];
+
+y1 = [8.7 23 52 67.2 72 73 73 72.8 72.8 72.8 66 42.8];
+
+y2 = [1.04 3.8 16.24 28.8 34.2 34.2 34.2 34.2 34.2 34 33.2 26.2];
+
+figure
+plot(x,y1)
+
+hold on 
+plot(x,y2)
+
+legend('10k', '1k');
+
+xlabel("Hz");
+ylabel("Av");
+
+hold off
+
+%%
+syms s 
+x4 = ilaplace(12/(s^2+s+12))
+
+%%
+
+roots([4 20 22])
+
+%%
+
+0.013/100
